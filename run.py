e03137e3645774ac52752fd8d84be84639e053c6
Brandan Neises <brandan.neises@gmail.com>
2022-08-01 14:03:36 -0500

    Final touches to templates

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 import datetime
 import yaml
 from flask import Flask, render_template
+from werkzeug.debug import DebuggedApplication
+
+
 app = Flask(__name__)
+app.debug = True
+app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
 
 @app.route('/')
 def tori():
-    context = {}
-    try:
-        with open('data.yaml', 'r') as data:
-            context = yaml.safe_load(data)
-        output = render_template('index.html.j2', cr_years=get_cr_years(), context=context)
-    except Exception as e:
-        output = render_template('Test error', e)
-    return output
+    with open('templates/data.yaml', 'r') as data:
+        context = yaml.safe_load(data)
+    return render_template('index.html.j2', cr_years=get_cr_years(), context=context)
 
 def get_cr_years(start=2020):
     current_year = datetime.datetime.now().year
