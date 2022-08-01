ccfa128b8854b45c6f644f24c439d88dea98256e
Brandan Neises <brandan.neises@gmail.com>
2022-08-01 15:00:40 -0500

    Remove debug mode

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ from werkzeug.debug import DebuggedApplication
 
 
 app = Flask(__name__)
-app.debug = True
-app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
+# app.debug = True
+# app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
 
 @app.route('/')
 def tori():
